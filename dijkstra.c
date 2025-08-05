73a6217f6dda22904a39766a0adc37a16368dfc9
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2025-08-05 10:50:09 -0300

    opaopa

--- dijkstra.c
+++ dijkstra.c
@@ -3,36 +3,42 @@
 
 int dijkstra(int grafo[MAX][MAX], int n, int origem, int destino, int proibidas[MAX]) {
     int dist[MAX];
+    int pred[MAX];
     int visitado[MAX] = {0};
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) {
         dist[i] = INF;
+        pred[i] = -1;
+    }
 
     dist[origem] = 0;
 
     for (int count = 0; count < n; count++) {
-        int u = -1;
+        int cidadeAtual = -1;
         int menorDist = INF;
 
         for (int i = 1; i <= n; i++) {
             if (!visitado[i] && dist[i] < menorDist) {
                 menorDist = dist[i];
-                u = i;
+                cidadeAtual = i;
             }
         }
 
-        if (u == -1) break;
-        visitado[u] = 1;
+        if (cidadeAtual == -1) break;
+        visitado[cidadeAtual] = 1;
 
-        for (int v = 1; v <= n; v++) {
-            if (grafo[u][v] != INF && !visitado[v]) {
-                if (proibidas[v] && v != destino) continue;
-                if (proibidas[u] && u != origem) continue;
+        for (int cidadeVizinha = 1; cidadeVizinha <= n; cidadeVizinha++) {
+            if (grafo[cidadeAtual][cidadeVizinha] != INF && !visitado[cidadeVizinha]) {
+                if (proibidas[cidadeVizinha] && cidadeVizinha != destino) continue;
+                if (proibidas[cidadeAtual] && cidadeAtual!= origem) continue;
 
-                if (dist[u] + grafo[u][v] < dist[v])
-                    dist[v] = dist[u] + grafo[u][v];
+                if (dist[cidadeAtual] + grafo[cidadeAtual][cidadeVizinha] < dist[cidadeVizinha]) {
+                    dist[cidadeVizinha] = dist[cidadeAtual] + grafo[cidadeAtual][cidadeVizinha];
+                    pred[cidadeVizinha] = cidadeAtual;
+                }
             }
         }
     }
+    
     return dist[destino];
 }
--- grafo.c
+++ grafo.c
@@ -8,12 +8,12 @@ void inicializaGrafo(int grafo[MAX][MAX], int n) {
 }
 
 void lerGrafo(int grafo[MAX][MAX], int m) {
-    int u, v, w;
+    int cidade1, cidade2, tamanho_estrada;
     for (int i = 0; i < m; i++) {
-        scanf("%d %d %d", &u, &v, &w);
-        if (w < grafo[u][v]) {
-            grafo[u][v] = w;
-            grafo[v][u] = w;
+        scanf("%d %d %d", &cidade1, &cidade2, &tamanho_estrada);
+        if (tamanho_estrada < grafo[cidade1][cidade2]) {
+            grafo[cidade1][cidade2] = tamanho_estrada;
+            grafo[cidade2][cidade1] = tamanho_estrada;
         }
     }
 }
